ce8023ebe28c2b4c94f1f3a467d357b302b31ee0
Connor <96927046+CrimsonHere@users.noreply.github.com>
2023-12-17 23:13:14 -0600

    Update Ex.js
    Added url sanitizer method to block bogus query or path request.

--- Ex.js
+++ Ex.js
@@ -11,7 +11,8 @@ const REQUEST_STORAGE = {};
 const CONNECTION_STORAGE = {}; 
 
 app.use(requestIp.mw()); // You will need the to be above the requestLimiter & connectionLimiter or It can't grab ip!!!
-app.use(requestLimiter)
+app.use(urlsanitizer);
+app.use(requestLimiter);
 app.use(connectionLimiter);
 
 // Request limiting function
@@ -74,6 +75,44 @@ function connectionLimiter(req, res, next) {
             delete CONNECTION_STORAGE[userIP];
         }
     });
+}
+
+// Got the idea from null Thanks bro.
+// URL & Query sanitizer function.
+function urlsanitizer(req, res, next) {
+    // Extract query parameters and URL path from the request
+    const requestQuery = req.query;
+    const urlPath = req.path;
+    // Iterate over each query parameter
+    for (const key in requestQuery) {
+        let value = requestQuery[key];
+        // Check if the query parameter matches any of the defined patterns
+        if (checkForPatterns(key + '=' + value)) {
+            // If a pattern is matched, return a 403 Forbidden response
+            return res.status(403).json({ error: 'Invalid query detected!' });
+        }
+    }
+
+    // Check if the URL path matches any of the defined patterns
+    if (checkForPatterns(urlPath)) {
+        // If a pattern is matched in the path, return a 403 Forbidden response
+        return res.status(403).json({ error: 'Invalid path detected!' });
+    }
+
+    // If no patterns are matched, continue to the next middleware
+    next();
+}
+
+function checkForPatterns(str)
+{
+    // Define regular expression patterns to detect suspicious strings
+    const pattern1 = /=([0-9]{9,})/;
+    const pattern2 = /([0-9]{12,})=([0-9]{12,})/;
+    const pattern3 = /[0-9]{0,18}[a-zA-Z0-9]{3}[0-9]{18}[a-zA-Z0-9]{1}/;
+    const pattern4 = /\/[0-9]{10,19}/;
+
+    // Test the string against each pattern
+    return pattern1.test(str) || pattern2.test(str) || pattern3.test(str) || pattern4.test(str);
 }
 
 // I will be adding more for stuff like headers verifacation and IP Abuse rating when I fell like it.
